d5409155748084c2cb461c075498c550800e6723
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-30 18:21:04 +0530

    Added: To store all user details in an array

--- AddressBook/AddressBookData.js
+++ AddressBook/AddressBookData.js
@@ -1,20 +1,27 @@
-// Importing personInfo class file.
 const PersonInfo = require("./PersonInfoData.js");
 const prompt = require('prompt-sync')();
 
-//Ability to add user-defined values to the PersonInfoData
+class AddressBookData {
+    personInfoList = new Array();
+    insert(personInfoList) {
+        try{
+            let personInfo = new PersonInfo();
+            personInfo.fName = prompt("Enter First Name : "); 
+            personInfo.lName = prompt("Enter Last Name : "); 
+            personInfo.addressName = prompt("Enter Address : "); 
+            personInfo.cityName = prompt("Enter City : "); 
+            personInfo.stateName = prompt("Enter State : "); 
+            personInfo.zipNumber = prompt("Enter Zip : "); 
+            personInfo.phoneNumberInput = prompt("Enter Phone Number : "); 
+            personInfo.emailInput = prompt("Enter Email : "); 
 
-try{
-    let personInfo = new PersonInfo();
-    personInfo.fName = prompt("Enter First Name : "); 
-    personInfo.lName = prompt("Enter Last Name : "); 
-    personInfo.addressName = prompt("Enter Address : "); 
-    personInfo.cityName = prompt("Enter City : "); 
-    personInfo.stateName = prompt("Enter State : "); 
-    personInfo.zipNumber = prompt("Enter Zip : "); 
-    personInfo.phoneNumberInput = prompt("Enter Phone Number : "); 
-    personInfo.emailInput = prompt("Enter Email : "); 
-    console.log(personInfo.toString());
-} catch ( e ) {
-    console.error(e);
+            personInfoList.push(personInfo);
+
+            return personInfoList;
+        } catch ( e ) {
+            console.error(e);
+        }
+    }
 }
+
+module.exports = AddressBookData;

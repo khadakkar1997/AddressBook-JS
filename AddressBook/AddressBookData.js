31f16dd8ac238475a50a7d900e3693816790ebb9
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-30 18:18:34 +0530

    Added: To validate user details

--- AddressBook/AddressBookData.js
+++ AddressBook/AddressBookData.js
@@ -2,17 +2,19 @@
 const PersonInfo = require("./PersonInfoData.js");
 const prompt = require('prompt-sync')();
 
-
-var personInfo = new PersonInfo("Samarth", "BM", "Layout", "DVG", "Ktaka", "577004", "9874563214", "sam@gmail.com");
-console.log(personInfo.toString());
-
 //Ability to add user-defined values to the PersonInfoData
-personInfo.firstName = prompt("Enter First Name : "); 
-personInfo.lastName = prompt("Enter Last Name : "); 
-personInfo.address = prompt("Enter Address : "); 
-personInfo.city = prompt("Enter City : "); 
-personInfo.state = prompt("Enter State : "); 
-personInfo.zip = prompt("Enter Zip : "); 
-personInfo.phoneNumber = prompt("Enter Phone Number : "); 
-personInfo.email = prompt("Enter Email : "); 
-console.log(personInfo.toString());
+
+try{
+    let personInfo = new PersonInfo();
+    personInfo.fName = prompt("Enter First Name : "); 
+    personInfo.lName = prompt("Enter Last Name : "); 
+    personInfo.addressName = prompt("Enter Address : "); 
+    personInfo.cityName = prompt("Enter City : "); 
+    personInfo.stateName = prompt("Enter State : "); 
+    personInfo.zipNumber = prompt("Enter Zip : "); 
+    personInfo.phoneNumberInput = prompt("Enter Phone Number : "); 
+    personInfo.emailInput = prompt("Enter Email : "); 
+    console.log(personInfo.toString());
+} catch ( e ) {
+    console.error(e);
+}
